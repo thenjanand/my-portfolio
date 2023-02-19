08a76092bd2271ede5b6240ca8cf59bdd214cdbb
NEERAJ ANAND <thenjanand@gmail.com>
2023-02-19 20:12:06 +0530

    adding resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/DIVYANSH.pdf";
+import pdf from "../../Assets/../Assets/Neeraj's_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://github.com/thedivyanshu/Myportfolio/blob/main/src/Assets/DIVYANSH.pdf";
+  "https://github.com/thenjanand/my-portfolio/tree/main/src/Assets/Neeraj's_Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
